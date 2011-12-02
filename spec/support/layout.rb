b89b5f885da9c79d756aae870a9abfacd3b4f572
Johan Sveholm <jsveholm@gmail.com>
2011-12-02 18:21:08 +0900

    id -> tag_id

--- spec/support/layout.rb
+++ spec/support/layout.rb
@@ -2,12 +2,12 @@ def have_alert(s); have_flash(s,:alert) end
 def have_error(s); have_css("p.inline-errors",:text=>s) end
 def have_flash(s,type); have_css("div#flash_#{type}",:text=>s) end
 def have_notice(s); have_flash(s,:notice) end
-def id(s,tag); ids(tag).select{|e| e=~/#{s}/}.first end
-def ids(tag); all(:css, tag.to_s).map{|e| e[:id]} end
-def li(s); find(:css, "li##{id(s,:li)}") end
+def li(s); find(:css, "li##{tag_id(s,:li)}") end
 def table(row=nil, col=nil)
   table = all(:css, "table tr").map{|e| e.all(:css, "td").map(&:text)}.reject{|e| e.empty?}
   return table if row.nil?
   return table[row] if col.nil?
   return table[row][col]
 end
+def tag_id(s,tag); tag_ids(tag).select{|e| e=~/#{s}/}.first end
+def tag_ids(tag); all(:css, tag.to_s).map{|e| e[:id]} end
