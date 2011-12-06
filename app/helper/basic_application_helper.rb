aa505569e68afe8fe426fde4ecba120045e270df
Johan Sveholm <jsveholm@gmail.com>
2011-12-06 18:06:59 +0900

    some updates

--- app/helper/basic_application_helper.rb
+++ app/helper/basic_application_helper.rb
@@ -1,10 +1,11 @@
 module BasicApplicationHelper
+  def minititle(s); raw "<h2>#{s}</h2>" end
   def new(s); t('labels.new',:o=>t(s)) end
   def pt(s); t(s).pluralize end
-  def subtitle(subtitle); raw "<h3>#{subtitle}</h3>" end
+  def subtitle(s); raw "<h3>#{s}</h3>" end
   def sure?; t('messages.sure?') end
-  def title(title)
-    content_for(:title){ title.to_s }
-    raw "<h1>#{title}</h1>"
+  def title(s)
+    content_for(:title){ s.to_s }
+    raw "<h1>#{s}</h1>"
   end
 end
--- spec/support/layout.rb
+++ spec/support/layout.rb
@@ -1,3 +1,4 @@
+def have_a_table(id); have_css("table##{id}") end
 def have_alert(s); have_flash(s,:alert) end
 def have_blank_error; have_error("can't be blank") end
 def have_error(s); have_css("p.inline-errors",:text=>s) end
@@ -5,11 +6,12 @@ def have_flash(s,type); have_css("div#flash_#{type}",:text=>s) end
 def have_notice(s); have_flash(s,:notice) end
 def have_title(s); have_css("h1",:text=>s) end
 def li(s); find(:css, "li##{tag_id(s,:li)}") end
-def table(row=nil, col=nil)
-  table = all(:css, "table tr").map{|e| e.all(:css, "td").map(&:text)}.reject{|e| e.empty?}
+def table(id="", row=nil, col=nil)
+  table = tables(id).map{|e| e.all(:css,"tr").map{|f| f.all(:css,"td").map(&:text)}}.first.reject{|e| e.empty?}
   return table if row.nil?
   return table[row] if col.nil?
   return table[row][col]
 end
+def tables(id=""); id.blank? ? all(:css,"table") : all(:css,"table##{id.to_s}") end
 def tag_id(s,tag); tag_ids(tag).select{|e| e=~/#{s}/}.first end
 def tag_ids(tag); all(:css, tag.to_s).map{|e| e[:id]} end
