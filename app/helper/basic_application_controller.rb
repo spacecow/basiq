a2750f0180fa0e98e1f96713b3b4befefcbeacc3
Johan Sveholm <jsveholm@gmail.com>
2012-01-31 13:17:09 +0900

    canceled and placed

--- app/helper/basic_application_controller.rb
+++ app/helper/basic_application_controller.rb
@@ -1,5 +1,6 @@
 module BasicApplicationController
-  def alertify(s); t("alerts.#{s}") end
+  def alertify(s) t("alerts.#{s}") end
+  def canceled(s) t('successes.canceled',:o=>t(s)) end
   def created(s,i=0,pl=nil)
     if i == 1 
       t('successes.created_nos',:o=>t(s),:i=>1,:v=>t(:was))
@@ -17,6 +18,7 @@ module BasicApplicationController
   def not_created(s)
     t("alerts.not_created",:o=>t(s))
   end
+  def placed(s) t('successes.placed',:o=>t(s)) end
   def pt(s); t(s).pluralize end
   def updated(s); t("successes.updated",:o=>t(s)) end
 
