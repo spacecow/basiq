449c1c398df4be7c9c88532cef6672e5ab64c82e
Johan Sveholm <jsveholm@gmail.com>
2012-02-08 13:33:33 +0900

    tj

--- app/helper/basic_application_controller.rb
+++ app/helper/basic_application_controller.rb
@@ -1,41 +1,45 @@
 module BasicApplicationController
   def added_to_cart(o,name,i=1)
     if i==1
-      t("added_to_cart.one", o:pl(o,i), name:name, count:i, :scope=>:successes)
+      jt("added_to_cart.one", o:pl(o,i), name:name, count:i, :scope=>:successes)
     else
-      t("added_to_cart.other", o:pl(o,i), name:name, count:i, :scope=>:successes)
+      jt("added_to_cart.other", o:pl(o,i), name:name, count:i, :scope=>:successes)
     end
   end
-  def alertify(s) t("alerts.#{s}") end
-  def canceled(s) t('successes.canceled',:o=>t(s)) end
-  def confirmed(s) t('successes.confirmed',:o=>t(s)) end
+  def alertify(s) jt("alerts.#{s}") end
+  def canceled(s) jt('successes.canceled',:o=>jt(s)) end
+  def confirmed(s) jt('successes.confirmed',:o=>jt(s)) end
   def created(o,i=nil)
     i ? succ_no(:created,o,i) : succ(:created,o)
   end
   def created_adv(o,name); succ_adv(:created,o,name) end
   def created_state(mdl,state)
-    t('successes.created_state',:o=>t(mdl),:state=>t(state).downcase)
+    jt('successes.created_state',:o=>jt(mdl),:state=>jt(state).downcase)
   end
-  def deleted(s) t('successes.deleted',:o=>t(s)) end
+  def deleted(s) jt('successes.deleted',:o=>jt(s)) end
   def deleted_adv(o,name); succ_adv(:deleted,o,name) end
-  def emptied(s) t('successes.emptied',:o=>t(s)) end
-  def notify(s) t("notices.#{s}") end
+  def emptied(s) jt('successes.emptied',:o=>jt(s)) end
+  def jt(s,*opt)
+    TRANSLATION_LOG.debug s
+    t(s,opt)
+  end
+  def notify(s) jt("notices.#{s}") end
   def not_created(o)
-    t(:not_created, o:pl(o,1), :scope=>:alerts)
+    jt(:not_created, o:pl(o,1), :scope=>:alerts)
   end
   def pl(s,i=2)
     if i==1 
-      t("#{s}.one",count:i) =~ /translation missing/ ? t(s) : t("#{s}.one",count:i)
+      jt("#{s}.one",count:i) =~ /translation missing/ ? jt(s) : jt("#{s}.one",count:i)
     else
-      t("#{s}.other",count:i)
+      jt("#{s}.other",count:i)
     end
   end
-  def pt(s) t(s).pluralize end
+  def pt(s) jt(s).pluralize end
   def removed_from_cart(o,name,i=1)
     if i==1
-      t("removed_from_cart.one", o:pl(o,i), name:name, count:i, :scope=>:successes)
+      jt("removed_from_cart.one", o:pl(o,i), name:name, count:i, :scope=>:successes)
     else
-      t("removed_from_cart.other", o:pl(o,i), name:name, count:i, :scope=>:successes)
+      jt("removed_from_cart.other", o:pl(o,i), name:name, count:i, :scope=>:successes)
     end
   end
   def updated(o) succ(:updated,o) end
@@ -50,16 +54,16 @@ module BasicApplicationController
   private
 
     def succ(lbl,o)
-      t(lbl, o:pl(o,1), :scope=>:successes) 
+      jt(lbl, o:pl(o,1), :scope=>:successes) 
     end
     def succ_adv(lbl,o,name)
-      t(lbl, o:pl(o,1), name:name, :scope=>:successes_adv)
+      jt(lbl, o:pl(o,1), name:name, :scope=>:successes_adv)
     end
     def succ_no(lbl,o,i)
       if i==1
-        t("#{lbl}.one", o:pl(o,i), count:i, :scope=>:successes_no)
+        jt("#{lbl}.one", o:pl(o,i), count:i, :scope=>:successes_no)
       else
-        t("#{lbl}.other", o:pl(o,i), count:i, :scope=>:successes_no)
+        jt("#{lbl}.other", o:pl(o,i), count:i, :scope=>:successes_no)
       end
     end
 end
