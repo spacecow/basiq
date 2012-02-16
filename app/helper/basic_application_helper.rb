2ecbe9831da6e8a83b10b2e597d03b1555382b02
Johan Sveholm <jsveholm@gmail.com>
2012-02-16 17:05:49 +0900

    added source field

--- app/helper/basic_application_helper.rb
+++ app/helper/basic_application_helper.rb
@@ -6,13 +6,18 @@ module BasicApplicationHelper
   def create(o,i=1) labels(:create,o,i) end
   def edit(o,i=1) labels(:edit,o,i) end
   def empty(s); jt('labels.empty',:o=>jt(s)) end
+  def formtitle(mdl)
+    s = mdl.class.to_s.downcase
+    subtitle(mdl.new_record? ? new(s) : edit(s))
+  end
   def ft(s); jt("formtastic.labels.#{s.to_s}") end
   def mess(s,*opt) jt("messages.#{s}",*opt) end
   def minititle(s); raw "<h2>#{s}</h2>" end
   def new(o,i=1) labels(:new,o,i) end
   def remove(o,i=1) labels(:remove,o,i) end
   def submit(f,mdl)
-    f.submit mdl.new_record? ? create(mdl.class.to_s.downcase) : update(mdl.class.to_s.downcase) 
+    s = mdl.class.to_s.downcase
+    f.submit mdl.new_record? ? create(s) : update(s) 
   end
   def subtitle(s); raw "<h3>#{s}</h3>" end
   def sure?; mess(:sure?) end
