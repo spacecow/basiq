c3b240a89074e4a0d7d8fcef11f5368a8ad3a6d0
Johan Sveholm <jsveholm@gmail.com>
2012-07-03 17:13:23 +0900

    something

--- spec/support/session.rb
+++ spec/support/session.rb
@@ -1,22 +1,3 @@
-#def login(user="test",password="secret")
-def login(userid="test",password="secret")
-  visit login_path
-  fill_in "Login", :with => userid 
-  fill_in "Password", :with => password 
-  click_button "Login"
-end
-
-def login_admin 
-  user = create_admin(:email=>'admin@example.com')
-  login('admin@example.com')
-  user
-end
-def login_member 
-  user = create_member(:email=>'member@example.com')
-  login('member@example.com')
-  user
-end
-
 def create_user(user)
   FactoryGirl.create(:user,username:user)
 end
