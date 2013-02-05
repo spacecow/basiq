bf233ec1a9cca55fed9d5d758c5b34ef10850c66
Johan Sveholm <jsveholm@gmail.com>
2013-02-05 17:05:21 +0900

    added present

--- app/helper/basic_application_helper.rb
+++ app/helper/basic_application_helper.rb
@@ -15,6 +15,25 @@ module BasicApplicationHelper
   def mess(s,*opt) t("messages.#{s}",*opt) end
   def minititle(s); raw "<h2>#{s}</h2>" end
   def new(o,i=1) labels(:new,o,i) end
+  def present(array, klass=nil)
+    if array.instance_of? Array
+      object = array.shift
+      parent = array.shift
+      grandparent = array.shift
+    else
+      object = array
+      parent = nil
+      grandparent = nil
+    end
+    if object.class.superclass.superclass.to_s == "ActiveRecord::Base"
+      klass ||= "#{object.class.superclass}Presenter".constantize
+    else
+      klass ||= "#{object.instance_of?(Class) ? object : object.class}Presenter".constantize
+    end
+    presenter = klass.new(object, parent, grandparent, self)
+    yield presenter if block_given?
+    presenter
+  end
   def remove(o,i=1) labels(:remove,o,i) end
   def submit(f,mdl)
     s = mdl.class.to_s.downcase
